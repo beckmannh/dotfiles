5cd2fd4d7a9638561cc10ee442a32170f65b3de2
Henrique <hiquebeckmann1@outlook.com>
2018-08-25 21:37:56 -0300

    modify i3bar and qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -7,14 +7,17 @@ c.colors.tabs.odd.fg = '#999999'
 c.colors.tabs.selected.even.bg = '#161616'
 c.colors.tabs.selected.odd.bg = '#161616'
 c.colors.tabs.selected.odd.fg = '#ffffff'
+c.colors.statusbar.insert.bg = '#000000'
+c.colors.statusbar.url.success.https.fg = '#ffffff'
+c.colors.statusbar.url.hover.fg = '#ffffff'
 
 c.editor.command = ['gvim', '{file}']
 c.editor.encoding = 'utf-8'
 c.input.insert_mode.auto_load = False
 c.scrolling.bar = False
 c.downloads.position = 'bottom'
-c.statusbar.hide = True
+c.statusbar.hide = False
 c.statusbar.position = 'top'
 c.tabs.position = 'top'
-c.url.searchengines = {'DEFAULT': 'https://www.google.com.br/search?q={}','ddg': 'https://duckduckgo.com/?q={}','gi': 'https://images.google.com/?q={}'}
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}','g': 'https://www.google.com.br/search?q={}','gi': 'https://images.google.com/?q={}','wiki': 'https://wiki.archlinux.org/index.php?search={}','olx': 'http://pa.olx.com.br/?q={}'}
 config.bind(';R', 'config-source')
